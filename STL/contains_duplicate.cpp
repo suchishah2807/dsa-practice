ed0c3a4c0e4b3bdc173129507a1068b93ed51957
Suchi Shah <suchikeyurshah@gmail.com>
2025-12-25 22:38:33 +0530

    contains duplicate leetcode question

---
+++ STL/contains_duplicate.cpp
@@ -0,0 +1,16 @@
+//leetcode problem 217, unordered set
+#include <iostream>
+using namespace std;
+const int N=1e5+10;
+int a[N];
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        int num;
+        cin>>num;
+        a[i]=num;
+        
+    }
+}
