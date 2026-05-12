5c76b7eec40c229a512af37c7137a404eb3fe1f3
Suchi Shah <suchikeyurshah@gmail.com>
2026-05-12 14:45:13 +0530

    delete a node from linked list with NO head provided

---
+++ arrays/linked_list/delete_just_node.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next; //pointer to the next element
+    
+    public:
+    Node(int data1, Node* next1){
+        data=data1;
+        next=next1;
+    }
+    public:
+    Node(int data1){
+        data=data1;
+        next=nullptr; //for the last element the pointer of next element is null pointer
+    }
+};
+
+Node* convertarrtoLL(vector <int> &arr){
+    Node* head = new Node(arr[0]); // creates a new node with value of arr[0] and next=null pointer
+    Node* mover = head; //this is a pointer that tranverses through array
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void deleteNode(Node* node) {
+        node->data=node->next->data;
+        node->next=node->next->next;
+    }
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}
