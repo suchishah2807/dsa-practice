e7c07af2d0c9924646825595746e138e43453fc9
Suchi Shah <suchikeyurshah@gmail.com>
2026-05-18 19:26:14 +0530

    basic calculator stack problem

---
+++ STL/basic_calculatorII.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int calculate(string s) {
+    stack <int> st;
+    int n=0;
+    char op = '+';
+    s+='+';
+    for(char c : s){
+        if(c==' ') continue;
+        if(isdigit(c)){
+            n = n*10 + (c - '0');
+            continue;
+        }
+        if(op=='+') st.push(n);
+        else if(op=='-') st.push(-n);
+        else if(op=='*'){
+            int top = st.top(); st.pop();
+            st.push(top*n);
+        }
+        else if(op=='/'){
+            int top = st.top(); st.pop();
+            st.push(top/n);
+        }
+        op=c;
+        n=0;
+    }
+    int res = 0;
+    while(!st.empty()){
+        res += st.top();
+        st.pop();
+    }
+return res;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}
